c25d5f670f5b3e48d2eece5c5b910f9113e8a5cc
arhipovmn <arhipovmn@gmail.com>
2024-02-19 07:29:24 +0000

    it's easier

--- logger.py
+++ logger.py
@@ -1,11 +1,10 @@
 import logging
-import datetime
 from config import config
 
 def logger():
     logger = logging.getLogger()
     logging.basicConfig(
-        filename='logs/'+datetime.datetime.now().strftime('%Y-%m-%d')+'_IP-'+config['IP-address-for-cam']+'_channel-'+config['channel-id-for-cam']+'.log',
+        filename='logs/camNotification.log',
         level=logging.DEBUG,
         datefmt='%Y-%m-%d %H:%M:%S',
         format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
